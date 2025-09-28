7ae8fb2ca7dcdc1333283149e85590f317aa94be
King <steveamaza@gmail.com>
2025-09-28 23:49:13 +0100

    refactor: simplify reverseString implementation by removing unnecessary function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,14 +1,5 @@
 const reverseString = function(input) {
-    function reverseArray(statement) {
-        let newStatement = [];
-        for(let index=statement.length-1; index>=0; index--) {
-            newStatement.push(statement[index])
-        }
-        return newStatement;
-    }
-    return reverseArray(input.split(" "))
-    .map((word) => reverseArray(word).join(""))
-    .join(" ");
+    return input.split("").reverse().join("")
 };
 
 // Do not edit below this line
