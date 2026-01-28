88956ae162ed5a2552d5a3a057b25e952f3da3b7
King <steveamaza@gmail.com>
2026-01-28 14:32:31 +0100

    fix: define parameter for palindromes function to check for palindrome correctly

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,10 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    let splitInput = input.replace(/ /g, "").toLowerCase().split("");
+    let splitInputReversed = splitInput.toReversed();
 
-};
+    return splitInput.every((ch, i) => ch === splitInputReversed[i]);
+
+    }
 
 // Do not edit below this line
 module.exports = palindromes;
